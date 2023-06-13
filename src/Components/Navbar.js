ed1b828d5b7ceca0d5c23f78945a48e785947ee4
nishantkr02 <nishant.nik.02@gmail.com>
2023-06-13 19:27:13 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,17 +4,17 @@ function Navbar() {
   return (
     <div className='container-fluid shadow p-2'>
       <nav className="navbar navbar-expand-lg navbar-light bg-tertiary shadow  " >
-          <a className="navbar-brand text-primary h2 bold" href="#">YourNews</a>
+          <a className="navbar-brand text-primary h2 bold" href="/NewsPage">YourNews</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="/NewsPage">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link" href="/NewsPage">Link</a>
               </li>
             </ul>
             <form className="d-flex">
