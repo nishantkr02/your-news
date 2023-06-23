69e7127e1fa4271325a9784082becb04d4e28d7c
nishantkr02 <nishant.nik.02@gmail.com>
2023-06-23 18:44:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes,Route ,Link} from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import NewsPage from "./Components/NewsPage";
 import About from "./Components/About";
@@ -12,11 +12,13 @@ function App() {
     <div className="alert alert-danger alert-dismissible fade show p-2 shadow-lg" role="alert">
       <p className="text-danger h5 m-auto text-center">
   <strong>   Hi there..!</strong> This Version of the App has some.. "Complications" . Please go to  
-  <a  href="/About" style={{textDecoration:"none"}}> About  </a>  Page and find Out .
+  <Link  to="/About" style={{textDecoration:"none", color:"black"}}> About  </Link>  Page and find Out .
   <button type="button" className="close btn btn-outline-secondary mx-2 my-2 " data-bs-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">Dismiss &times;</span>
-  </button>
-  </p>
+  </button> </p>
+ 
+    <p className="m-auto text-center text-secondary"><strong>Note:</strong>This app is not smartphone screen frinendly yet ,kindly switch to a Desktop or toggle desktop Mode </p>
+ 
 </div>
 </div>
     <Routes>
